5d8ce0d722d57c2d3973b2a0a7767b22ff4a8fbc
caleb <clayton.caleb@gmail.com>
2015-04-26 10:51:18 +0100

    client impl now receives udp connection info, loads song from disk, breaks song into bytes, sends bytes via udp

--- src/AudioStreamClientImpl.java
+++ src/AudioStreamClientImpl.java
@@ -1,18 +1,27 @@
 package src;
 
+import java.io.BufferedInputStream;
 import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
 import java.io.DataOutputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
-import java.util.Scanner;
 
 public class AudioStreamClientImpl implements AudioStreamClient {
-	private Socket clientSocket;
 	private int id;
 	private boolean isProvider;
 	
+	private Socket clientSocket;
+	private BufferedInputStream in;
+	private ByteArrayOutputStream out;
+		
 	public AudioStreamClientImpl(){
 		try {
 			clientSocket = new Socket("localhost", AudioStreamServerImpl.DEFAULT_TCP_PORT);
@@ -47,6 +56,28 @@ public class AudioStreamClientImpl implements AudioStreamClient {
 					new BufferedReader( new InputStreamReader(clientSocket.getInputStream()));
 			id = Integer.parseInt(inputFromServer.readLine());
 			isProvider = (inputFromServer.readLine().equals("1"));
+			System.out.println("isProv: " + isProvider);
+			int udpPort = Integer.parseInt(inputFromServer.readLine());
+			System.out.println("Received: " + udpPort);
+			DatagramSocket udpSocket = new DatagramSocket();
+			String ip =inputFromServer.readLine();
+			InetAddress IPAddress = InetAddress.getByName(ip);
+			
+			if (isProvider){
+				System.out.println("Is provider");
+				loadSong();
+				//send audio
+				while (true){
+					byte[] bytes = getNextAudioBytes();
+					DatagramPacket sendPacket =
+								new DatagramPacket(bytes, bytes.length, IPAddress, udpPort);
+						udpSocket.send(sendPacket);
+					}
+				}
+			else {
+				//receive and play audio
+			}
+			udpSocket.close();
 			inputFromServer.close();
 			outputToServer.close();
 			clientSocket.close();
@@ -56,8 +87,40 @@ public class AudioStreamClientImpl implements AudioStreamClient {
 		return true;
 	}
 
+	
+
 	@Override
 	public void run() {
 		connect();		
+	}
+	
+	private void loadSong() {
+		try {
+			in = new BufferedInputStream(new FileInputStream("test.mp3"));
+			out = new ByteArrayOutputStream();
+		} catch (FileNotFoundException e) {
+				e.printStackTrace();
+		} 
+	}
+
+	private byte[] getNextAudioBytes() {
+		byte[] buffer = new byte[AudioStreamServerImpl.BUFFER_SIZE];
+		try {
+			int read = in.read(buffer);
+			
+			if (read == -1) {
+				loadSong();
+				getNextAudioBytes();
+			}
+			out.write(buffer);
+			
+			byte[] nextBytes = out.toByteArray();
+			out.reset();
+			return nextBytes;
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
 	}
 }
