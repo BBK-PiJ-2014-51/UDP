2ba497136d54541d98317f0acf09bccc4214075e
caleb <clayton.caleb@gmail.com>
2015-04-26 10:45:00 +0100

    updated client interface with comments. added client test to check if client can submit audio to the server's audio buffer

--- src/AudioStreamClient.java
+++ src/AudioStreamClient.java
@@ -2,9 +2,26 @@ package src;
 
 public interface AudioStreamClient extends Runnable {
 
+	/**
+	 * Instructs client to connect to server. Receive connection
+	 * information and begin participation in audio streaming.
+	 * 
+	 * @return false if cannot connect
+	 */
 	public boolean connect();
 
+	/**
+	 * Returns id issued by server
+	 * 
+	 * @return unique id issued by server
+	 */
 	int getId();
 
+	/**
+	 * Returns role in audio stream. True represents provider of audio, while
+	 * false indicates the client will be receiving and playing the audio.
+	 * 
+	 * @return boolean indicating if client is sending audio
+	 */
 	boolean isAudioProvider();
 }
--- tests/AudioStreamClientTest.java
+++ tests/AudioStreamClientTest.java
@@ -1,8 +1,9 @@
 package tests;
 
-import static org.junit.Assert.*;
-
+import static org.junit.Assert.assertEquals;
 import org.junit.Test;
+
+import java.io.IOException;
 
 import src.AudioStreamClient;
 import src.AudioStreamClientImpl;
@@ -31,6 +32,40 @@ public class AudioStreamClientTest {
 			if (i == 0 && !client.isAudioProvider()) unexpected = true;
 			else if (i==1 && client.isAudioProvider()) unexpected = true;
 		}
+		server.closeTcpService();
 		assertEquals(unexpected, false);	
+	}
+	
+	/**
+	 * Checks that the first client correctly receives an id, provider status, 
+	 * udp port to send audio, and address to send audio. Then sends some audio.
+	 * 
+	 * Performs check by looking at the first object in the buffer and monitoring it 
+	 * for a change.
+	 * 
+	 * @throws IOException 
+	 * @throws InterruptedException 
+	 * 
+	 */
+	@Test
+	public void testProvidesAudio() throws IOException, InterruptedException{
+		
+		AudioStreamServer server = new AudioStreamServerImpl();
+		Thread sT = new Thread(server);
+		sT.start();
+		
+		byte[] data = server.getAudioByte(0);
+		String originalObject = data.toString();
+		
+		AudioStreamClient client = new AudioStreamClientImpl();
+		Thread clientTh = new Thread(client);
+		clientTh.start();
+		
+		Thread.sleep(5);
+		
+		data = server.getAudioByte(0);
+		String updatedObject = data.toString();
+		
+		assertEquals(false, originalObject.equals(updatedObject));
 	}
 }
